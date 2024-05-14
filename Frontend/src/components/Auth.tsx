f56d71cdb53d2c0a7e58ea192029078bc3c3027d
Sahil Khachi <sahilkhachi95@gmail.com>
2024-05-15 00:45:24 +0530

    login signup completed

--- Frontend/src/components/Auth.tsx
+++ Frontend/src/components/Auth.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+export const Auth = ({ type }: { type: "signup" | "signin" }) => {
+    return (
+        <>
+            <div className='flex justify-center align-center h-screen '>
+                <form className='flex flex-col justify-center w-3/6'>
+                    <div>
+                        {type == "signup" ? <div>
+                            <div className='text-3xl font-black'>Create an account</div>
+                            <div className='max-w-md text-xl font-medium text-slate-400'>
+                                Already an user? <a href="#">Signin</a>
+                            </div>
+                        </div>
+                            : <div>
+                                <div className='text-3xl font-black'>Login an account</div>
+                                <div className='max-w-md text-xl font-medium text-slate-400'>
+                                    Not an user? <a href="#">Signup</a>
+                                </div>
+                            </div>}
+                    </div>
+                    <label className='text-xl font-semibold mt-5'>Username</label>
+                    <input className='border border-gray-300 p-3 my-3 rounded-md ' type='text' placeholder='Enter your username' />
 
-export const Auth = () =>{
-  return (
-    <div>
-      
-    </div>
-  )
+                    <label className='text-xl font-semibold mt-3' >Email</label>
+                    <input className='border border-gray-300 p-3 my-3 rounded-md' type='email' placeholder='me@example.com' />
+
+                    <label className='text-xl font-semibold mt-3'>Password</label>
+                    <input className='border border-gray-300 p-3 my-3 rounded-md' type='password' placeholder='******' />
+
+                    <input
+                        className="bg-black mt-5 border border-black text-white font-semibold text-lg rounded-md py-3 px-6 shadow-md focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200 transition duration-300 hover:bg-transparent hover:border-outset-black hover:text-black"
+                        type="submit"
+                        value={type === "signin" ? "Sign in" : "Sign up"}
+                    />
+
+
+                </form>
+            </div>
+        </>
+    )
 }
 
 
--- Frontend/src/pages/Signin.tsx
+++ Frontend/src/pages/Signin.tsx
@@ -1,9 +1,15 @@
+import { Auth } from "../components/Auth"
+import { Quote } from "../components/Quote"
 
 export const Signin = () => {
-    return (
+  return <div>
+    <div className="grid grid-cols-1 lg:grid-cols-2">
       <div>
-        HEY there
+        <Auth type="signin" />
       </div>
-    )
-  }
-  
+      <div className="hidden lg:block">
+        <Quote />
+      </div>
+    </div>
+  </div>
+}
