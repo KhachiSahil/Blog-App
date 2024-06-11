eb1aa9905ebd1ffa861ac4fcac6b26054078b4c7
Sahil Khachi <sahilkhachi95@gmail.com>
2024-06-11 22:40:16 +0530

    q

--- Frontend/src/components/Auth.tsx
+++ Frontend/src/components/Auth.tsx
@@ -14,6 +14,7 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
 
     async function sendRequest() {
         try {
+            console.log(BACKEND_URL)
             const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`, postInputs);
             const jwt = response.data;
             localStorage.setItem("token", jwt);
