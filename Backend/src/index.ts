f70cf85f37ce1026c012b6445b2817f519dc1056
Sahil Khachi <sahilkhachi95@gmail.com>
2024-06-11 22:36:01 +0530

    simple updates

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,17 +1,22 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 import { userRouter } from './routes/user';
 import { blogRouter } from './routes/blog';
-import { cors } from 'hono/cors'
+import { cors } from 'hono/cors';
 
 export const app = new Hono<{
   Bindings: {
-      DATABASE_URL: string;
-      JWT_SECRET: string;
+    DATABASE_URL: string;
+    JWT_SECRET: string;
   }
 }>();
 
-app.use('/*', cors())
-app.route('/api/v1/user', userRouter)
-app.route('/api/v1/blog', blogRouter)
+app.use('/*', cors({
+  origin: 'https://blog-app-rose-one.vercel.app', 
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowHeaders: ['Content-Type', 'Authorization'],
+}));
 
-export default app
+app.route('/api/v1/user', userRouter);
+app.route('/api/v1/blog', blogRouter);
+
+export default app;
